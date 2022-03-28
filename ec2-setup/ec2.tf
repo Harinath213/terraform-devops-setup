ace603e1ce6089a1d3acdccefa78d288217eb0ed
Harinath213 <konepalli.009@outlook.com>
2022-03-28 23:51:50 +0530

    added user data file

--- ec2-setup/ec2.tf
+++ ec2-setup/ec2.tf
@@ -8,10 +8,17 @@ data "aws_ami" "basic-app" {
   
 }
 
+data "template_file" "user_data" {
+  template = file("${path.module}/user_data.sh")
+  
+}
+
+
 resource "aws_instance" "web-server" {
     ami       = data.aws_ami.basic-app.id
     instance_type = "t2.micro"
     iam_instance_profile = aws_iam_instance_profile.ec2-app.id
+    user_data            = data.template_file.user_data.rendered
 
     tags = {
         name = "web-server"
